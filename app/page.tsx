99c832259f0592a754a941de48b09c7b7f17b695
garold664 <garold664@gmail.com>
2024-07-01 12:51:20 +0300

    creates home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { Poppins } from 'next/font/google';
 import Image from 'next/image';
 
+const font = Poppins({
+  subsets: ['latin'],
+  weight: ['600'],
+});
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]  from-sky-500 to-sky-400 ">
+      <div className="space-y-6 text-center">
+        <h1 className={cn('text-6xl font-semibold text-white drop-shadow-md')}>
+          🔒Auth
+        </h1>
+        <p className="text-white text-lg ">Authentication service</p>
+        <Button variant={'secondary'} size={'xl'}>
+          Sign in
+        </Button>
+      </div>
+    </main>
+  );
 }
