513380acbdb7b869f4a3766b7e51d3fb38960200
garold664 <garold664@gmail.com>
2024-07-02 10:52:52 +0300

    creates helper functions to find user in db by email and id

---
+++ data/user.ts
@@ -0,0 +1,26 @@
+import { db } from '@/lib/db';
+
+export const getUserByEmail = async (email: string) => {
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    return email;
+  } catch (error) {
+    null;
+  }
+};
+export const getUserById = async (id: string) => {
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        id,
+      },
+    });
+    return user;
+  } catch (error) {
+    null;
+  }
+};
