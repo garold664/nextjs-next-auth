9a7d8f1829a760988a7f605e79b848a3e290b018
garold664 <garold664@gmail.com>
2024-07-03 10:54:16 +0300

    adds role field to User model

--- auth.ts
+++ auth.ts
@@ -10,6 +10,17 @@ export const {
   signOut,
   auth,
 } = NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,
