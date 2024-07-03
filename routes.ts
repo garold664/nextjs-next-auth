98cc8a139f7c54e0b2a8eb8e8da3754dc87c2846
garold664 <garold664@gmail.com>
2024-07-03 07:29:38 +0300

    sets up the routes

---
+++ routes.ts
@@ -0,0 +1,29 @@
+/**
+ * An array of public routes
+ * These routes are accessible without authentication
+ * @type {string[]}
+ */
+
+export const publicRoutes = ['/'];
+
+/**
+ * An array of routes that used for authentication
+ * These routes will redirect to the settings page
+ * @type {string[]}
+ */
+export const authRoutes = ['/auth/login', '/auth/signup'];
+
+/**
+ * The prefix of routes that used for authentication
+ * Routes that start with this prefix are used for authentication purposes
+ * @type {string}
+ */
+
+export const apiAuthPrefix = '/api/auth';
+
+/**
+ * The default login redirect
+ * @type {string}
+ */
+
+export const DEFAULT_LOGIN_REDIRECT = '/settings';
