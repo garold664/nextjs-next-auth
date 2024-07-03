9aff035cef2ee0e347b74942fb7172a94ce0c107
garold664 <garold664@gmail.com>
2024-07-03 09:20:37 +0300

    finishes login functioallity

--- actions/login.ts
+++ actions/login.ts
@@ -1,8 +1,10 @@
 'use server';
 
 import { LoginSchema } from '@/schemas';
-import { error } from 'console';
+import { signIn } from '@/auth';
 import { z } from 'zod';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
+import { AuthError } from 'next-auth';
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
@@ -11,6 +13,37 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
       error: 'Invalid email or password',
       success: '',
     };
+
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+    return {
+      error: '',
+      success: 'You successfully logged in',
+    };
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return {
+            error: 'Invalid email or password',
+            success: '',
+          };
+        default:
+          console.log(error);
+          return {
+            error: 'Something went wrong',
+            success: '',
+          };
+      }
+    }
+    throw error;
+  }
   return {
     error: '',
     success: 'Email successfully sent',
