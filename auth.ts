ce016aaa20f04de029550947b659ccb5fe0d51a0
garold664 <garold664@gmail.com>
2024-07-03 15:55:57 +0300

    adds linkAccount event to authomatically verify email for OAuth accounts

--- auth.ts
+++ auth.ts
@@ -13,6 +13,18 @@ export const {
   signOut,
   auth,
 } = NextAuth({
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          emailVerified: new Date(),
+        },
+      });
+    },
+  },
   callbacks: {
     async signIn({ user }) {
       // if (!user.id) return false;
