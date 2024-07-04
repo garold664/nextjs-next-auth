a2513e182a5cab259a7f14dbb8d0801d94d85f4d
garold664 <garold664@gmail.com>
2024-07-04 16:32:34 +0300

    implements generating new verification token on user login

--- actions/login.ts
+++ actions/login.ts
@@ -5,6 +5,8 @@ import { signIn } from '@/auth';
 import { z } from 'zod';
 import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 import { AuthError } from 'next-auth';
+import { getUserByEmail } from '@/data/user';
+import { generateVerificationToken } from '@/lib/tokens';
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
@@ -15,6 +17,24 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     };
 
   const { email, password } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (!existingUser || !existingUser.email || !existingUser.password)
+    return {
+      error: 'User, Email or password does not exist',
+    };
+
+  if (!existingUser.emailVerified) {
+    const verificationToken = await generateVerificationToken(
+      existingUser.email
+    );
+
+    return {
+      success:
+        "We can't let you in until you verified your email. Confirmation email sent. If you don't see it, check your spam folder.",
+    };
+  }
 
   try {
     await signIn('credentials', {
